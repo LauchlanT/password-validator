e85af0a129fd24449ce7b3fef20c9ee0ef8d21f2
L Toal <lc790935@dal.ca>
2018-06-06 12:12:00 -0300

    Adjusted layout, changed button and textview text.

--- app/src/main/java/com/lauchlantoal/csci3130assignment2/PasswordInput.java
+++ app/src/main/java/com/lauchlantoal/csci3130assignment2/PasswordInput.java
@@ -2,6 +2,9 @@ package com.lauchlantoal.csci3130assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PasswordInput extends AppCompatActivity {
 
@@ -10,4 +13,19 @@ public class PasswordInput extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_input2);
     }
+
+    public void checkPassword(View view){
+        EditText textbox = findViewById(R.id.editText);
+        String password = textbox.getText().toString();
+        Validator checker = new Validator();
+        int strength = checker.validate(password);
+        String message = "Weak Password";
+        if(strength == 5){
+            message = "Strong Password";
+        }
+        TextView alert = findViewById(R.id.textView2);
+        alert.setText(message);
+        return;
+    }
+
 }
